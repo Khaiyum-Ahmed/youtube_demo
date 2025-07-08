5a42a192e790efa71c05d6d8680f5405624c7391
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-07-08 14:10:21 +0600

    all card dynamically time added

--- script/script.js
+++ script/script.js
@@ -1,6 +1,36 @@
 
 console.log('script added')
 
+// Time management
+
+function timeCalculation(time){
+    // const day = parseInt(time/86400);
+    // let remainingDay = time % 86400;
+    const hour = parseInt(time/3600);
+    let remainingSecond = time % 3600;
+    const minutes = parseInt(remainingSecond /60);
+    let seconds = parseInt(remainingSecond % 60);
+        // ${day} day
+    return `${hour} hours ${minutes} minutes ${seconds} seconds ago` ;
+}
+// console.log(timeCalculation(206959))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 1. fetch, load and show categories on html
 
 
@@ -57,20 +87,25 @@ const displayVideos = (videos) => {
         card.classList = "card"
         card.innerHTML = `
        
-  <figure class="h-[200px] rounded-xl">
+  <figure class="h-[200px] rounded-xl relative">
     <img class="h-full w-full object-cover"
       src="${video.thumbnail}"
       alt="Shoes" />
+      ${video.others.posted_date?.length === 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${timeCalculation(video.others.posted_date)}</span>`
+            };
+        
+
   </figure>
   <div class="px-0 py-2 flex gap-2">
-    <div class="w-[60px]">
-        <img class="w-14 h-14 rounded-full object-cover" src="${video.authors[0].profile_picture}" />
+    <div>
+        <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" />
     </div>
     <div>
     <h2 class="font-bold">${video.title}</h2>
     <div class="flex items-center gap-2">
         <p class="text-gray-400">${video.authors[0].profile_name}</p>
-        <img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" />
+        ${video.authors[0].verified ? `<img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" />` : ""}
+        
     </div>
     <p></p>
     </div>
---
+++ script/test.js
@@ -0,0 +1,29 @@
+
+// only for test 
+
+// const isVerified = true;
+
+// // if(isVerified === true){
+// //     console.log('user is verified')
+// // }
+// // else{
+// //     console.log('user is not verified')
+// // }
+
+// console.log(`${isVerified === true? 'user is verified':'user is not verified'}`)
+
+
+
+
+
+// function timeCalculation(time){
+//     const day = parseInt(time/86400);
+//     let remainingDay = time % 86400;
+//     const hour = parseInt(remainingDay/3600);
+//     let remainingSecond = time % 3600;
+//     const minutes = parseInt(remainingSecond /60);
+//     let seconds = parseInt(remainingSecond % 60);
+
+//     return `${day} day ${hour} hours ${minutes} minutes ${seconds} seconds ago` ;
+// }
+// console.log(timeCalculation(206959))
